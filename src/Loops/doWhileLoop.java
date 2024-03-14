59c8b3ea3fbd8e009035d8e5021fb8c8e7bf4b70
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-03-14 23:32:13 +0530

    do-While Loop

---
+++ src/Loops/doWhileLoop.java
@@ -0,0 +1,11 @@
+package Loops;
+
+public class doWhileLoop {
+    public static void main(String[] args) {
+        int i=0;
+        do {
+            System.out.println("Hello World using do while loop");
+            i++;
+        }while(i<5);
+    }
+}
