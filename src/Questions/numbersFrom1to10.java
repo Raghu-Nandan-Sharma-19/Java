b016ddadf2048a08e71b5c81d7ee7ade09bdeba6
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-03-14 23:22:01 +0530

    Print numbers from 1 to 10

--- src/Questions/numbersFrom1to10.java
+++ src/Questions/numbersFrom1to10.java
@@ -2,8 +2,8 @@ package Questions;
 
 public class numbersFrom1to10 {
     public static void main(String[] args) {
-        for(int i=0; i<=10; i++) {
-            System.out.println(i);
+        for(int i=1; i<=10; i++) {
+            System.out.print(i+" ");
         }
     }
 }
