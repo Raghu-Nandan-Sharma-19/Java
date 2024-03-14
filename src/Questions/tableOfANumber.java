f3e1977dda0f58c98a8265dfccc521b79a0a830e
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-03-14 23:41:57 +0530

    used for loop

--- src/Questions/tableOfANumber.java
+++ src/Questions/tableOfANumber.java
@@ -7,15 +7,8 @@ public class tableOfANumber {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter a number to print the table : ");
         int number = in.nextInt();
-        System.out.println(number+" * 1 = "+number*1);
-        System.out.println(number+" * 2 = "+number*2);
-        System.out.println(number+" * 3 = "+number*3);
-        System.out.println(number+" * 4 = "+number*4);
-        System.out.println(number+" * 5 = "+number*5);
-        System.out.println(number+" * 6 = "+number*6);
-        System.out.println(number+" * 7 = "+number*7);
-        System.out.println(number+" * 8 = "+number*8);
-        System.out.println(number+" * 9 = "+number*9);
-        System.out.println(number+" * 10 = "+number*10);
+        for(int i=1; i<=10; i++) {
+            System.out.println(number+" x "+i+" = "+(number*i));
+        }
     }
 }
