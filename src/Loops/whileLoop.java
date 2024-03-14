f01adee81af0c8ead8e02df05c12cb788cfb0c0c
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-03-14 23:29:47 +0530

    while Loop

---
+++ src/Loops/whileLoop.java
@@ -0,0 +1,13 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class whileLoop {
+    public static void main(String[] args) {
+        int i=0;
+        while(i<5) {
+            System.out.println("Hello World using while loop");
+            i++;
+        }
+    }
+}
