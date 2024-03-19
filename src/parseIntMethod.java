e9f344502f911df161242caa13e1df9fb2a4512e
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-03-19 20:11:35 +0530

    Parse Int and To String method

---
+++ src/parseIntMethod.java
@@ -0,0 +1,7 @@
+public class parseIntMethod {
+    public static void main(String[] args) {
+        String numbers = "123";
+        int number = Integer.parseInt(numbers);
+        System.out.println(number);
+    }
+}
---
+++ src/toStringMethod.java
@@ -0,0 +1,8 @@
+public class toStringMethod {
+    public static void main(String[] args) {
+        int number = 123;
+        String numbers = Integer.toString(number);
+        System.out.println("String is : "+number);
+        System.out.println("Length of string is : "+numbers.length());
+    }
+}
