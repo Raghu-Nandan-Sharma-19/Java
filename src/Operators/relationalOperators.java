7b7aeb2899403118784da8c3af6499d7a69cf0bd
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-03-21 20:23:51 +0530

    Relational operators

---
+++ src/Operators/relationalOperators.java
@@ -0,0 +1,19 @@
+package Operators;
+
+public class relationalOperators {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        // equals to
+        System.out.println(a==b); // false
+        // not equals to
+        System.out.println(a!=b); // true
+        // greater than
+        System.out.println(a>b); // true
+        // less than
+        System.out.println(a<b); // false
+        //  greater than equal to
+        System.out.println(a>=b); // true
+        // less than equal to
+        System.out.println(a<=b); // false
+    }
+}
