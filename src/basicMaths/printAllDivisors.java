d96b91ace475244baac8bec8b1601e95ea776c60
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-03-24 00:30:54 +0530

    Print all divisors

--- src/basicMaths/printAllDivisors.java
+++ src/basicMaths/printAllDivisors.java
@@ -1,14 +1,28 @@
 package basicMaths;
 
-import java.util.Scanner;
+import java.util.*;
+
+import static java.lang.Math.sqrt;
 
 public class printAllDivisors {
     public static void printDivisors(int n) {
+//        Alternate way
         for(int i=1; i<=n; i++) {
             if(n%i==0) {
-                System.out.println(i);
+                System.out.print(i+" ");
             }
         }
+        /*ArrayList<Integer> ls = new ArrayList<>();
+        for(int i=1; i<=sqrt(n); i++) {
+            if(n%i==0) {
+                ls.add(i);
+            }
+            if(n/i!=i) {
+                ls.add(n/i);
+            }
+        }
+        Collections.sort(ls);
+        System.out.println(ls);*/
     }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
