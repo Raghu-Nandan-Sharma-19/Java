666aa44954425e27876b46f8d528db24e9430252
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-03-28 20:08:09 +0530

    Check if a string is a palindrome

--- src/recursion/checkStringIsPalindrome.java
+++ src/recursion/checkStringIsPalindrome.java
@@ -3,22 +3,23 @@ package recursion;
 import java.util.Scanner;
 
 public class checkStringIsPalindrome {
-    public static String reverseString(String str, int i) {
-        if(i==str.length()) {
-            return "";
+    public static boolean reverseString(String str, int i) {
+        if(i>=str.length()/2) {
+            return true;
         }
-        return reverseString(str, i+1)+str.charAt(i);
+        if(str.charAt(i)!=str.charAt(str.length()-i-1)) {
+            return false;
+        }
+        return reverseString(str, i+1);
     }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter a string : ");
         String str = in.nextLine();
-        System.out.println("Reverse of string is : ");
-        System.out.println(reverseString(str, 0));
-        if (str.equals(reverseString(str, 0))) {
-            System.out.println("String is palindrome");
+        if(reverseString(str, 0)) {
+            System.out.println("String is palindrome.");
         } else {
-            System.out.println("String is not palindrome");
+            System.out.println("String is not palindrome.");
         }
     }
 }
