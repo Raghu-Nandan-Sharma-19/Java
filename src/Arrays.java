e0056621b966cb408563dc716bb2f56d0e4a59c6
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-06-29 22:41:48 +0530

    Update Arrays.java

--- src/Arrays.java
+++ src/Arrays.java
@@ -7,7 +7,7 @@ public class Arrays {
 //        int[] marks = new int[3];
 //        marks[0] = 97;
 //        marks[1] = 98;
-//        marks[2] = 95;
+//        marks[2] = 85;
 
         // Second way
 //        int[] marks = {98, 97, 95};
