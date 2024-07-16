db7ad7f11e0581326d234386c2c1c9d3da60b465
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-07-16 13:42:53 +0530

    Method Overloading

---
+++ src/oops/MethodOverloading.java
@@ -0,0 +1,24 @@
+package oops;
+
+import java.util.Scanner;
+
+public class MethodOverloading {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Greet greet1 = new Greet();
+        greet1.greetings();
+        Greet greet2 = new Greet();
+        System.out.println("Enter name : ");
+        String name = in.nextLine();
+        greet2.greetings(name);
+        in.close();
+    }
+    static class Greet {
+        void greetings() {
+            System.out.println("Hello, Good Morning");
+        }
+        void greetings(String name) {
+            System.out.println("Hello " + name + ", Good Morning");
+        }
+    }
+}
