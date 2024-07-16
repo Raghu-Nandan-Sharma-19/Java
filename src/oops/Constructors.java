1815a63787783cd3ba879b1b89d2e46d390c88f0
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-07-16 14:17:31 +0530

    Constructors

---
+++ src/oops/Constructors.java
@@ -0,0 +1,26 @@
+package oops;
+
+import java.util.Scanner;
+
+public class Constructors {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter real number : ");
+        int real = in.nextInt();
+        System.out.println("Enter imaginary number : ");
+        int imaginary = in.nextInt();
+        Complex num1 = new Complex(real, imaginary);
+        num1.print();
+        in.close();
+    }   
+    static class Complex {
+        int a, b;
+        Complex(int real, int imaginary) {
+            a = real;
+            b = imaginary;
+        }
+        void print() {
+            System.out.println("Complex number is : " + a + " + " + b + "i");
+        }
+    }
+}
