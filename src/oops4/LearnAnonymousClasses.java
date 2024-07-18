bc40a94f1f22f4d012e7e625b1189d255310b5ad
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-07-19 01:46:41 +0530

    Anonymus classes

--- src/oops4/LearnAnonymousClasses.java
+++ src/oops4/LearnAnonymousClasses.java
@@ -1,19 +1,18 @@
 package oops4;
 
 public class LearnAnonymousClasses {
-    static OuterClass obj1 = new OuterClass() {
-        public void outerMethod() {
-            System.out.println("Hi, I am outer method");
-        }
-    }; 
-
-    static SuperInterface obj2 = new SuperInterface() {
-        @Override
-        public void interfaceMethod() {
-            System.out.println("Hi, I am interface method");
-        }
-    };
     public static void main(String[] args) {
+        OuterClass obj1 = new OuterClass() {
+            public void outerMethod() {
+                System.out.println("Hi, I am outer method");
+            }
+        }; 
+        SuperInterface obj2 = new SuperInterface() {
+            @Override
+            public void interfaceMethod() {
+                System.out.println("Hi, I am interface method");
+            }
+        };
         obj1.outerMethod();
         obj2.interfaceMethod();
     }
