e1c2a2f9367241e2cf835d458ddb3be3152145e6
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-07-20 02:34:54 +0530

    Modified maximum number in array.java

--- src/problems/MaximumNumberInArray.java
+++ src/problems/MaximumNumberInArray.java
@@ -5,19 +5,24 @@ import java.util.Scanner;
 public class MaximumNumberInArray {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+
         System.out.println("Enter size of the array : ");
         int size = in.nextInt();
+        
         int[] numbers = new int[size];
+        int maximumNumber = Integer.MIN_VALUE;
         System.out.println("Enter " + size + " numbers : ");
+
         for (int i = 0; i < size; i++) {
             numbers[i] = in.nextInt();
         }
-        int maximumNumber = Integer.MIN_VALUE;
+
         for (int number : numbers) {
-            if (number > maximumNumber) {
+            if(number > maximumNumber) {
                 maximumNumber = number;
             }
         }
+
         System.out.println("Maximum number in array is : " + maximumNumber);
         in.close();
     }
