a91e3b078f4832864a7bd146c920fa928c865c53
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-07-22 20:31:36 +0530

    Wrapper Class

---
+++ src/genericsAndWrapperClass/WrapperClass.java
@@ -0,0 +1,14 @@
+package genericsAndWrapperClass;
+
+public class WrapperClass {
+    public static void main(String[] args) {
+        
+        Integer obj1 = 12;  // autoboxing
+
+        int age = obj1;  // unboxing
+
+        System.out.println(obj1);
+        
+        System.out.println(age);
+    }    
+}
