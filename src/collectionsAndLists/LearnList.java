44f87e2202da6528a93686fabb5222f9450238f4
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-07-23 12:52:17 +0530

    Lists

--- src/collectionsAndLists/LearnList.java
+++ src/collectionsAndLists/LearnList.java
@@ -1,19 +1,20 @@
 package collectionsAndLists;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
-public class LearnArrayList {
+public class LearnList {
     public static void main(String[] args) {
         List<Integer> list1 = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>();
+        List<Integer> list2 = new LinkedList<>();
         list1.add(10);
         list1.add(1);
         list1.add(20);
         list2.add(20);
         list2.add(30);
         System.out.println("List 1 is : " + list1);
-        System.out.printf("Size of list 1 is : " + list1.size());
+        System.out.println("Size of list 1 is : " + list1.size());
         System.out.println("Does list 1 contains 200 ? " + list1.contains(200));
         list1.remove(Integer.valueOf(1));
         System.out.println("Removed 1 from list 1 : " + list1);
